b2b0efb33a34124594befacc1b80959b568af2fb
anduser <anduser@DeveloperPC004.(none)>
2016-06-29 18:23:50 +0300

    3

--- popup-button/popup-button.component.js
+++ popup-button/popup-button.component.js
@@ -1,19 +1,19 @@
 angular.
-    module('myApp').
-    component('popupButton', {
-        bindings: {
-            onClick: '&'
-        },
-        template:
-        '<div>' +
-            '<a class="button" href="#" ng-click="ctrl.openPopup">Pop up</a>' +
-        '</div>',
-        controller: ['$scope', function MyController($scope) {
-            var self = this;
-            //$ctrl = this;
-            /*self.openPopup = function openPopup() {
-                //console.log(self.parent.status);
-                //self.parent.status = true;
-            };*/
-        }]
+    module('popupButton')
+    .directive('popupButton', function() {
+        return {
+            restrict: 'EA',
+            template:
+            '<div>' +
+                '<a class="button" href="#" ">Pop up</a>' +
+            '</div>',
+            controller: ['$scope', function MyController($scope) {
+                var self = this;
+                //$ctrl = this;
+                /*self.openPopup = function openPopup() {
+                 //console.log(self.parent.status);
+                 //self.parent.status = true;
+                 };*/
+            }]
+        }
     });
