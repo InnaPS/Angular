7d4029692c9c213f834960e78d187bd263cfa35f
anduser <anduser@DeveloperPC004.(none)>
2016-06-29 18:23:05 +0300

    3

--- container/container.component.js
+++ container/container.component.js
@@ -1,17 +1,23 @@
 angular.
-    module('myApp').
-    component('myContainer', {
+    module('myContainer')
+    .directive('myContainer', function () {
+        return {
+            restrict: 'EA',
+            templateUrl:  'container/my-container.html'
 
-        controller: ['$scope', function MyController($scope) {
-            /*var containerCtrl = this;
-            containerCtrl.status = {
-                value: true
-            };*/
-            $scope.flag = true;
-            /*containerCtrl.openPopup = function openPopup() {
-                containerCtrl.status = true;
-                console.log(containerCtrl.status);
-            };*/
-            console.log( $scope.flag);
-        }]
+        }
     });
+
+
+
+    /*function MyController($scope){
+ /!*var containerCtrl = this;
+ containerCtrl.status = {
+ value: true
+ };*!/
+ $scope.flag = true;
+ /!*containerCtrl.openPopup = function openPopup() {
+ containerCtrl.status = true;
+ console.log(containerCtrl.status);
+ };*!/
+ console.log($scope.flag);*/
