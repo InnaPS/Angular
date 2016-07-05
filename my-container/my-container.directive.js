a7bcc96a6a071c22d01616a18d014b5cd37da722
anduser <anduser@DeveloperPC004.(none)>
2016-07-05 18:44:35 +0300

    9

--- my-container/my-container.directive.js
+++ my-container/my-container.directive.js
@@ -4,7 +4,7 @@ angular
         return {
             restrict: 'EA',
             templateUrl:  'my-container/my-container.template.html',
-            controller: function($scope) {
+            controller: function($scope, $http) {
                 $scope.items = [
                     {
                         key: 'aaa',
@@ -30,7 +30,78 @@ angular
                 /*$scope.closePopup = function() {
                     $scope.status = false;
                 };*/
-            }
+                /*$http.get("graphs.json").then(function(response){
+                    $scope.graphs = response.data.graphObjs;
+                    console.log($scope.graphs);
+                });*/
+                $scope.graphs = [
+                    {
+                        "id":"001",
+                        "links":[
+                            "003",
+                            "004",
+                            "007"
+                        ],
+                        "color":"yellow"
+                    },
+                    {
+                        "id":"002",
+                        "links":[
+                            "003",
+                            "005"
+                        ],
+                        "color":"green"
+                    },
+                    {
+                        "id":"003",
+                        "links":[
+                            "001",
+                            "002",
+                            "006",
+                            "007"
+                        ],
+                        "color":"red"
+                    },
+                    {
+                        "id":"004",
+                        "links":[
+                            "001"
+                        ],
+                        "color":"blue"
+                    },
+                    {
+                        "id":"005",
+                        "links":[
+                            "002",
+                            "007"
+                        ],
+                        "color":"teal"
+                    },
+                    {
+                        "id":"006",
+                        "links":[
+                            "003",
+                            "007"
+                        ],
+                        "color":"pink"
+                    },
+                    {
+                        "id":"007",
+                        "links":[
+                            "001",
+                            "003",
+                            "005",
+                            "006"
+                        ],
+                        "color":"coral"
+                    }
+                ];
+
+                $scope.drawDefault = function(){
+
+                };
+            }/*,
+            templateNamespace: 'svg'*/
 
         }
     });
